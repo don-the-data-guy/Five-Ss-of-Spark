d986bda83fcdca6a2c2e993ca0e9f880f87c1fc6
DonTheDataGuy <68694934+don-the-data-guy@users.noreply.github.com>
2023-12-06 08:59:19 -0600

    Experiment #5136A
    Experiment #5136A

---
+++ Experiment #5136A.py
@@ -0,0 +1,62 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC <table>
+# MAGIC   <tr>
+# MAGIC     <td></td>
+# MAGIC     <td>VM</td>
+# MAGIC     <td>Quantity</td>
+# MAGIC     <td>Total Cores</td>
+# MAGIC     <td>Total RAM</td>
+# MAGIC     <td>IOPS/R-MBps/W-MBps</td>
+# MAGIC   </tr>
+# MAGIC   <tr>
+# MAGIC     <td>Driver:</td>
+# MAGIC     <td>**Standard_D13_v2**</td>
+# MAGIC     <td>**1**</td>
+# MAGIC     <td>**8 cores**</td>
+# MAGIC     <td>**56 GB**</td>
+# MAGIC     <td></td>
+# MAGIC   </tr>
+# MAGIC   <tr>
+# MAGIC     <td>Workers:</td>
+# MAGIC     <td>**Standard_D13_v2**</td>
+# MAGIC     <td>**10**</td>
+# MAGIC     <td>**80 cores**</td>
+# MAGIC     <td>**560 GB**</td>
+# MAGIC     <td>**24,000 / 375 / 187**</td>
+# MAGIC   </tr>
+# MAGIC </table>
+
+# COMMAND ----------
+
+# DBTITLE 1,Basic Initialization
+sc.setJobDescription("Step A: Basic initialization")
+
+# magic number that will produce 1 TB of data
+record_count = 15500000000
+
+# 1 GB per partition
+partitions = 512
+
+# COMMAND ----------
+
+# DBTITLE 1,Force Shuffle & Spill
+sc.setJobDescription("Step B: Shuffle w/Spill")
+from pyspark.sql.functions import sha2, col
+
+spark.conf.set("spark.sql.shuffle.partitions", partitions)
+
+df_1 = (spark
+  .range(0, record_count-1, 1, partitions)
+  .withColumnRenamed("id", "long_value_1")
+  .withColumn("hash_value_1", sha2(col("long_value_1").cast("string"), 256))
+)
+
+df_2 = (spark
+  .range(0, record_count, 1, partitions)
+  .withColumnRenamed("id", "long_value_2")
+  .withColumn("hash_value_2", sha2(col("long_value_2").cast("string"), 256))
+)
+
+df = df_1.join(df_2, col("long_value_1") == col("long_value_2"))
+df.write.format("noop").mode("overwrite").save()
